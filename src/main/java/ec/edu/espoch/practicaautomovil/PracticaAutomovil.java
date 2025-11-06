2cd418af48969bcba2ec3644f63cb68ca38f1630
Jair <jairguanoluisa@hotmail.com>
2025-11-06 14:27:02 -0500

    comentario

---
+++ src/main/java/ec/edu/espoch/practicaautomovil/PracticaAutomovil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practicaautomovil;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PracticaAutomovil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
