660874767cbd60490bd5f5b9c0f2222d2f2a2bf0
Jair <jairguanoluisa@hotmail.com>
2025-11-06 23:49:22 -0500

    commit

--- src/main/java/ec/edu/espoch/practicaautomovil/PracticaAutomovil.java
+++ src/main/java/ec/edu/espoch/practicaautomovil/PracticaAutomovil.java
@@ -1,5 +1,4 @@
 package ec.edu.espoch.practicaautomovil;
-
 import ec.edu.espoch.practicaautomovil.clase.Automovil;
 import ec.edu.espoch.practicaautomovil.enumeracion.Color;
 import ec.edu.espoch.practicaautomovil.enumeracion.TipoAutomovil;
@@ -10,16 +9,33 @@ public class PracticaAutomovil {
     public static void main(String[] args) {
         Automovil miAuto = new Automovil("TOYOTA","2023","2.0",TipoCombustible.GASOLINA,TipoAutomovil.COMPACTO,4,5,200,Color.ROJO,0);
         
+        miAuto.mostrarValores();
+        System.out.println("");
+        
+        System.out.println("Acelerando");
         miAuto.acelerar(100);
         System.out.println("Velocidad actual: " + miAuto.getVelocidadActual() + " km/h");
+        System.out.println("");
         
+        System.out.println("Acelerando");
         miAuto.acelerar(20);
         System.out.println("Velocidad actual: " + miAuto.getVelocidadActual() + " km/h");
+        System.out.println("");
         
+        System.out.println("Desacelerando");
         miAuto.desacelerar(50);
         System.out.println("Velocidad actual: " + miAuto.getVelocidadActual() + " km/h");
+        System.out.println("");
         
+        System.out.println("TIEMPO (Prueba)");
+        double tiempoCalculado = miAuto.calcularTiempoEstimado(350.0);
+        if (tiempoCalculado != Double.POSITIVE_INFINITY) {
+            System.out.println("El tiempo estimado es: " + tiempoCalculado + " horas");
+        
+        System.out.println("Frenando");
         miAuto.frenar();
         System.out.println("Velocidad actual: " + miAuto.getVelocidadActual() + " km/h");
+        System.out.println("");
+        }
     }
 }
